ba86edefdcc34eb5f9707d370109759f56666317
Celta02 <r.espinoza.tagle@gmail.com>
2023-08-23 02:54:51 -0400

    MAIN MENU MOCKUP
    * Created a Main Menu Scene.
    * Added Buttons:
    - Play - To Start Game moving to the main scene
    - Settings - Not currently working
    - Controls - Not Currently Working
    - Exit - Exit the app.

---
+++ Assets/_01_SCRIPTS/PlayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CeltaGames
+{
+    public class PlayButton : MonoBehaviour
+    {
+        public void OnPlayButtonClicked() => SceneManager.LoadScene(1);
+    }
+}
