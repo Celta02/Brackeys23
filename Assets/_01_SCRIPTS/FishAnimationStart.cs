217f7536d1ef7dbf64e511aa26bf379ad0db6322
Celta02 <r.espinoza.tagle@gmail.com>
2023-08-27 16:53:35 -0400

    FISHES
    * Now Fishes  can move in groups. Their animation starts when the player hits their box collider.
    * Stingray group implemented.
    * Clown-Fish group implemented.

---
+++ Assets/_01_SCRIPTS/FishAnimationStart.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace CeltaGames
+{
+    [RequireComponent(typeof(Animator), typeof(BoxCollider))]
+    public class FishAnimationStart : MonoBehaviour
+    {
+        Animator _animator;
+        Collider _collider;
+        static readonly int TriggerAction = Animator.StringToHash("TriggerAction");
+
+        void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _collider = GetComponent<Collider>();
+        }
+
+        void Start() => _collider.OnTriggerEnterAsObservable().Subscribe(StartAnimation).AddTo(this);
+
+        void StartAnimation(Collider col) => _animator.SetTrigger(TriggerAction);
+    }
+}
